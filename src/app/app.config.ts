3f8d6a787f75032cacc04e3941cdca0b99748e58
Bram Borggreve <borggreve@gmail.com>
2019-12-01 03:54:55 -0500

    feat: add auth pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,10 +10,11 @@ export const appLayout: UiLayout = {
   header: {
     style: 'dark',
     links: [
-      { path: 'home', label: 'Home', icon: 'mr-2 fa fa-fw fa-home' },
-      { path: 'docs', label: 'Docs', icon: 'mr-2 fa fa-fw fa-book' },
-      { path: 'blog', label: 'Blog', icon: 'mr-2 fa fa-fw fa-rss' },
-      { path: 'style-guide', label: 'Style Guide', icon: 'mr-2 fa fa-fw fa-paint-brush' },
+      { path: '/home', label: 'Home', icon: 'mr-2 fa fa-fw fa-home' },
+      { path: '/docs', label: 'Docs', icon: 'mr-2 fa fa-fw fa-book' },
+      { path: '/blog', label: 'Blog', icon: 'mr-2 fa fa-fw fa-rss' },
+      { path: '/login', label: 'Auth', icon: 'mr-2 fa fa-fw fa-lock' },
+      { path: '/style-guide', label: 'Style Guide', icon: 'mr-2 fa fa-fw fa-paint-brush' },
       { url: 'https://github.com/beeman/kikstart-ui-starter', label: 'GitHub', icon: 'mr-2 fa fa-fw fa-github' },
     ],
   },
